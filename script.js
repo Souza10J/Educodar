3c1638cbab3a62bf2ef058828d107be21f678a19
Souza <souza.de.joas@gmail.com>
2019-12-18 21:48:44 -0300

    Adicionado JS, melhorias no layout, remoção do hover, gforms em contatos.html. Rev 1.0

---
+++ script.js
@@ -0,0 +1,50 @@
+function aniOne(){
+	let animaDiv1 = document.getElementById("info1");
+	let animaH_1 = document.getElementById("hinfo1");
+	animaDiv1.style.backgroundColor = "#0C3855";
+	animaH_1.style.color = "#FFFFFF";
+}
+function aniTwo(){
+	let animaDiv2 = document.getElementById("info2");
+	let animaH_2 = document.getElementById("hinfo2");
+	animaDiv2.style.backgroundColor = "#0C3855";
+	animaH_2.style.color = "#FFFFFF";
+}
+function aniTree(){
+	let animaDiv3 = document.getElementById("info3");
+	let animaH_3 = document.getElementById("hinfo3");
+	animaDiv3.style.backgroundColor = "#0C3855";
+	animaH_3.style.color = "#FFFFFF";
+}
+function aniFour(){
+	let animaDiv4 = document.getElementById("info4");
+	let animaH_4 = document.getElementById("hinfo4");
+	animaDiv4.style.backgroundColor = "#0C3855";
+	animaH_4.style.color = "#FFFFFF";
+}
+function release(){
+	
+	let animaDiv1 = document.getElementById("info1");
+	let animaH_1 = document.getElementById("hinfo1");
+
+	let animaDiv2 = document.getElementById("info2");
+	let animaH_2 = document.getElementById("hinfo2");
+
+	let animaDiv3 = document.getElementById("info3");
+	let animaH_3 = document.getElementById("hinfo3");
+
+	let animaDiv4 = document.getElementById("info4");
+	let animaH_4 = document.getElementById("hinfo4");
+
+	animaDiv1.style.backgroundColor = "rgba(255, 255, 255, 0.918)";
+	animaH_1.style.color = "#0C3855";
+
+	animaDiv2.style.backgroundColor = "rgba(255, 255, 255, 0.918)";
+	animaH_2.style.color = "#0C3855";
+
+	animaDiv3.style.backgroundColor = "rgba(255, 255, 255, 0.918)";
+	animaH_3.style.color = "#0C3855";
+
+	animaDiv4.style.backgroundColor = "rgba(255, 255, 255, 0.918)";
+	animaH_4.style.color = "#0C3855";
+}
